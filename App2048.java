91858a70ee7694f3832291c58c09cb785ae4d991
Nattaphat <eakacit@gmail.com>
2023-02-18 20:32:23 +0700

    Adding some code in app2048.

---
+++ App2048.java
@@ -0,0 +1,20 @@
+import java.awt.FlowLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLayer;
+import javax.sqing.JLabel;
+
+public class App2048 {
+    private JFrame window;
+
+    private JLabel header;
+    pri
+    private JLabel[][] numMap;
+
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+package proj2048;
+public class Main {
+    public static void main(String[] args) {
+        App a = new App();
+        useit(a);
+    }
+    static void useit(App a){
+
+    }
+}
